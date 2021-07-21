589baadef843a4caeff7f50eaa240c93056f9455
HyeJun <solar3070@naver.com>
2021-07-22 07:09:08 +0900

    edit database

--- community/settings.py
+++ community/settings.py
@@ -91,10 +91,14 @@ WSGI_APPLICATION = 'community.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.environ.get('DB_NAME'),
+       'USER': os.environ.get('DB_USER'),
+       'PASSWORD': os.environ.get('DB_PASSWORD'),
+       'HOST': os.environ.get('DB_HOST'),
+       'PORT': '',
+   }
 }
 
 
